df1b8533447d9ad5e2c24842938e8122d761292a
CorentinDupont <dupont.corentin44@gmail.com>
2018-02-13 17:36:14 +0100

    SandBoxTuto

---
+++ Assets/JeuDeTHUD/Scripts/Nain.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nain : MonoBehaviour {
+
+    public GameObject casePosition;
+
+    public AudioSource audioSource;
+    public AudioClip attackSound;
+
+	// Use this for initialization
+	void Start () {
+        this.gameObject.transform.position = casePosition.transform.position;
+        audioSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
