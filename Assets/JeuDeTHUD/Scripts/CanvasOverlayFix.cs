b5aa206b3c4493f278c7ee8b2edf2e81f23c1018
Corentin Dupont <dupont.corentin44@gmail.com>
2018-05-05 09:59:19 +0200

    HUD Fix + button sound

---
+++ Assets/JeuDeTHUD/Scripts/CanvasOverlayFix.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasOverlayFix : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
