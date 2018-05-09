6b953edd4f5412ef89486817b625d713674f4e49
Corentin Dupont <dupont.corentin44@gmail.com>
2018-05-09 23:52:01 +0200

    Return button avatar panel main menu

--- Assets/JeuDeTHUD/Scripts/InGameInterface/InGameMenu.cs
+++ Assets/JeuDeTHUD/Scripts/InGameInterface/InGameMenu.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class InGameMenu : MonoBehaviour {
-
+    private void Start()
+    {
+        foreach (Transform boardBoxTransform in this.gameObject.transform)
+        {
+            GameObject theBoardBox = boardBoxTransform.gameObject;
+        }
+    }
 }
